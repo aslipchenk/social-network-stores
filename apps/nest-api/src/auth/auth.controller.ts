4be08387ab376509e53b70a5cb4f45720fc60c7d
Anton Slipchenko <anton.slipchenko@plus4u.net>
2021-10-26 20:30:53 +0300

    feature: complete basic authenthication using email

--- apps/nest-api/src/auth/auth.controller.ts
+++ apps/nest-api/src/auth/auth.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Post, Body, Get, Put, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Put, Param, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserDto } from '../user/user.dto';
 import { User } from '../entities/user.entity';
+import { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('registration')
-  async registration(@Body() registrateUserDto: UserDto): Promise<User> {
-    return await this.authService.registration(registrateUserDto);
+  async registration(@Body() registrateUserDto: UserDto, @Req() req: Request): Promise<User> {
+    const requestUrl = req.protocol + "://" + req.get("host")
+    return await this.authService.registration(registrateUserDto, requestUrl);
   }
 
   @Post('login')
@@ -21,9 +23,11 @@ export class AuthController {
   @Get('refresh')
   async refresh() {}
 
-  @Put('activate/:link')
-  async activate(@Param('link') link: string): Promise<void> {
-    return await this.authService.activate(link);
+  @Get('activate/:link')
+  async activate(@Param('link') link: string, @Res() res: Response): Promise<any> {
+    await this.authService.activate(link);
+    res.redirect("http://localhost:5000");
+    return { operation: "success" }
   }
 
   @Put('reset')
